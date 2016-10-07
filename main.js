8d9601abd92c2f8353428265ad1d69dec33c5063
Gustavo Sobral <ghsobral@gmail.com>
2016-10-06 22:55:34 -0300

    Configure pusher-js and electron-packager

--- main.js
+++ main.js
@@ -8,10 +8,7 @@ let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   mainWindow.on('closed', function () {
     mainWindow = null
@@ -19,3 +16,14 @@ function createWindow () {
 }
 
 app.on('ready', createWindow)
+
+const Pusher = require('pusher-js/node')
+
+const pusher = new Pusher('02cdc5ce216d575e7d41', {
+  encrypted: true
+})
+const channel = pusher.subscribe('test_channel')
+
+channel.bind('my_event', function(data) {
+  console.log(data.message)
+})
