d252c9031c37332e5823e3e549820020494245d5
Gustavo Sobral <ghsobral@gmail.com>
2016-10-14 22:29:06 -0300

    Add playlistDefault metadata

--- app/script.js
+++ app/script.js
@@ -17,6 +17,10 @@ $(document).ready(function() {
   $.material.init();
   $('#login-alert').hide();
 
+  $.getJSON("playlistDefault.json", function(data) {
+    localStorage.setItem('playlistDefault', JSON.stringify(data));
+  });
+
   $('form').on('submit', function() {
     event.preventDefault();
     $('#login-alert').hide();
