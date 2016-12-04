3fd9bc6660d262e500c01ad12e1d010363db3f46
Gustavo Sobral <ghsobral@gmail.com>
2016-12-04 02:02:11 -0300

    Show browser um application begin

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({center: true, width: 1300, height: 700, minWidth: 1300, minHeight: 700, fullscreenable: true, center: true})
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
+  mainWindow.webContents.openDevTools()
+
   mainWindow.on('closed', function () {
     mainWindow = null
   })
